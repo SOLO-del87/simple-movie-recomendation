da1d8ff7795fa199cc147dd7d7028b984bf945e8
SOLO-del87 <82113964+SOLO-del87@users.noreply.github.com>
2026-02-02 14:57:57 +0530

    Fixed broken streamlit config in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cd12c5576973c487b49ba5b2cfdcf0963beb740ce5264016cd616a4fe244ddb
-size 133
+oid sha256:246b5b428bb6c1697378589e6fdc7cc3c223ad165f4ca706f8a28021136dbb8a
+size 157
