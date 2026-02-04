08a1b331049e6b2c4c6ac598d2a8667dcb0a6358
SOLO-del87 <82113964+SOLO-del87@users.noreply.github.com>
2026-02-04 14:08:10 +0530

    initial commit

--- app.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:02c2ced4294aab382d8652ae58e3033d29786912553506f6cf2a5b15a4934d86
-size 1643
--- setup.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:246b5b428bb6c1697378589e6fdc7cc3c223ad165f4ca706f8a28021136dbb8a
-size 157
